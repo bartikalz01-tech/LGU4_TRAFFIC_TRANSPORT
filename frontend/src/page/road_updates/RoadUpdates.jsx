500a013963f28f6f030700d8999430a1f1339625
bartikalz01-tech <cruzmikko029@gmail.com>
2025-12-20 19:52:48 +0800

    Add the cctv videos

--- frontend/src/page/road_updates/RoadUpdates.jsx
+++ frontend/src/page/road_updates/RoadUpdates.jsx
@@ -1,7 +1,7 @@
-import { Sidebar } from "../sidebar/Sidebar";
+import { RoadUpdatesHeader } from "./RoadUpdatesHeader";
+import rightArrowIcon from '../../assets/arrows_right_line.svg';
 import './RoadUpdates.css';
 import '../../index.css'; 
-import { RoadUpdatesHeader } from "./RoadUpdatesHeader";
 
 export function RoadUpdates() {
 
@@ -10,8 +10,89 @@ export function RoadUpdates() {
       <RoadUpdatesHeader />
 
       <section className="cctv-container">
-        <div className="cctv-feed">Hi</div>
-        
+        <div className="cctv-feed">
+          <div className='cctv-info'>
+            <p>123 Street</p>
+            <div className="details-right-arrow">
+              <p>View Details</p>
+              <img src={rightArrowIcon} alt="Right arrow" className="icon-right" />
+            </div>
+          </div>
+
+          <div className="cctv-video">
+            <i class="fas fa-video"></i>
+          </div>
+        </div>
+
+        <div className="cctv-feed">
+          <div className='cctv-info'>
+            <p>124 Street</p>
+            <div className="details-right-arrow">
+              <p>View Details</p>
+              <img src={rightArrowIcon} alt="Right arrow" className="icon-right" />
+            </div>
+          </div>
+
+          <div className="cctv-video">
+            <i class="fas fa-video"></i>
+          </div>
+        </div>
+
+        <div className="cctv-feed">
+          <div className='cctv-info'>
+            <p>125 Street</p>
+            <div className="details-right-arrow">
+              <p>View Details</p>
+              <img src={rightArrowIcon} alt="Right arrow" className="icon-right" />
+            </div>
+          </div>
+
+          <div className="cctv-video">
+            <i class="fas fa-video"></i>
+          </div>
+        </div>
+
+        <div className="cctv-feed">
+          <div className='cctv-info'>
+            <p>126 Street</p>
+            <div className="details-right-arrow">
+              <p>View Details</p>
+              <img src={rightArrowIcon} alt="Right arrow" className="icon-right" />
+            </div>
+          </div>
+
+          <div className="cctv-video">
+            <i class="fas fa-video"></i>
+          </div>
+        </div>
+
+        <div className="cctv-feed">
+          <div className='cctv-info'>
+            <p>127 Street</p>
+            <div className="details-right-arrow">
+              <p>View Details</p>
+              <img src={rightArrowIcon} alt="Right arrow" className="icon-right" />
+            </div>
+          </div>
+
+          <div className="cctv-video">
+            <i class="fas fa-video"></i>
+          </div>
+        </div>
+
+        <div className="cctv-feed">
+          <div className='cctv-info'>
+            <p>128 Street</p>
+            <div className="details-right-arrow">
+              <p>View Details</p>
+              <img src={rightArrowIcon} alt="Right arrow" className="icon-right" />
+            </div>
+          </div>
+
+          <div className="cctv-video">
+            <i class="fas fa-video"></i>
+          </div>
+        </div>
       </section>
     </>
   );

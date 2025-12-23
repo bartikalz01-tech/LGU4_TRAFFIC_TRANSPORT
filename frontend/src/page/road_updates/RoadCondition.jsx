d3602e8debe6d13d8964dffdf0644bcfec6cf5d2
bartikalz01-tech <cruzmikko029@gmail.com>
2025-12-23 13:04:07 +0800

    Fix color palette, and add the logo from example  UI. The full vid cctv is still in progress.

--- frontend/src/page/road_updates/RoadCondition.jsx
+++ frontend/src/page/road_updates/RoadCondition.jsx
@@ -1,14 +1,19 @@
 import './RoadCondition.css';
 import '../../index.css';
+
 export function RoadCondition({ road, onClose }) {
 
   return(
     <>
       <div className='road-condition-overlay'>
         <div className='road-condition-content'>
-          <button onClick={onClose}>Close</button>
-          <h2>{road.name}</h2>
-          <p>Test</p>
+          <div className='road-title'>
+            <h2 className='road-name'>{road.roadName}</h2>
+            <button className='close-btn' onClick={onClose}>Close</button>
+          </div>
+          <div className='full-cctv-video'>
+            <i class="fas fa-video"></i>
+          </div>
         </div>
       </div>
     </>

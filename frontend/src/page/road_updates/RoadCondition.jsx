1335cff36c1771a9591a00e97d00eb5c1d9d6133
bartikalz01-tech <cruzmikko029@gmail.com>
2025-12-23 15:05:19 +0800

    Fixed the Road Condition Header and the cctv footage.

--- frontend/src/page/road_updates/RoadCondition.jsx
+++ frontend/src/page/road_updates/RoadCondition.jsx
@@ -1,3 +1,4 @@
+import hamburgerMenu from '../../assets/hamburger-menu.svg';
 import './RoadCondition.css';
 import '../../index.css';
 
@@ -7,9 +8,22 @@ export function RoadCondition({ road, onClose }) {
     <>
       <div className='road-condition-overlay'>
         <div className='road-condition-content'>
-          <div className='road-title'>
-            <h2 className='road-name'>{road.roadName}</h2>
-            <button className='close-btn' onClick={onClose}>Close</button>
+          <div className='cctv-options-container'>
+            <div className='road-title-menu'>
+              <img className='hamburger-menu-logo' src={hamburgerMenu} />
+              <h2 className='road-name'>{road.roadName}</h2>
+            </div>
+
+            <div className='cctv-controls'>
+              <span className='status-live'>LIVE</span>
+              {/*<button className='btn pause-btn'>Pause</button>*/}
+              {/*<button className='btn replay-btn'>Replay</button>*/}
+              <button className='btn record-btn'>Save Record footage</button>
+            </div>
+
+            <div className='right-side'>
+              <button className='close-btn' onClick={onClose}>Close</button>
+            </div>
           </div>
           <div className='full-cctv-video'>
             <i class="fas fa-video"></i>
